6cba21c86ee80031179231ccd6c1ecdff0b472d7
jamesdispatchfdsales-tech <jamesdispatch.fdsales@gmail.com>
2026-02-27 17:48:03 +0500

    Remove frontend submodule entry

--- ishine-frontend/src/app/(shop)/shop/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-import ProductDetail from "./ProductDetail";
-
-export default async function ProductDetailPage({ params }: { params: Promise<{ id: string }> }) {
-    const { id } = await params;
-    return <ProductDetail productId={id} />;
-}
